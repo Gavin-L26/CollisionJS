c6103d08c4390f80ed8f39bc20057eb343b7a5e1
GavinL26 <gavinuse00@gmail.com>
2020-12-19 21:36:04 -0400

    fixed landing page bug

--- example1.js
+++ example1.js
@@ -6,13 +6,6 @@ example1.addRect(0, 0, 100, 100, 80, 80);
 const example2 = new InteractiveObjects("#example2");
 example2.addCir(100, 100, 200, 200, 80, 1, "#900C3F");
 
-// const example3 = new InteractiveObjects("#example3", 0.8);
-// example3.addCir(550, 50, -150, 80, 50, 30);
-// example3.addCir(20, 300, 200, -90, 20);
-// example3.addCir(50, 250, 220, -80, 20);
-// example3.addCir(80, 350, 230, -100, 20);
-// example3.addCir(100, 200, 250, -70, 20);
-
 // const example4 = new InteractiveObjects("#example4", 0.6, true);
 // example4.addCir(15, 50, 0, 0, 20);
 // example4.addCir(60, 50, 0, 0, 20);
