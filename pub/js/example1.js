3147c5df0e6e16853a55c61acf4229f8bca83907
GavinL26 <gavinuse00@gmail.com>
2020-12-19 22:43:41 -0400

    deleted unused code

--- pub/js/example1.js
+++ pub/js/example1.js
@@ -5,15 +5,3 @@ example1.addRect(0, 0, 100, 100, 80, 80);
 
 const example2 = new InteractiveObjects("#example2");
 example2.addCir(100, 100, 200, 200, 80, 1, "#900C3F");
-
-// const example4 = new InteractiveObjects("#example4", 0.6, true);
-// example4.addCir(15, 50, 0, 0, 20);
-// example4.addCir(60, 50, 0, 0, 20);
-// example4.addCir(105, 50, 0, 0, 20);
-// example4.addCir(150, 50, 0, 0, 20);
-// example4.addCir(195, 50, 0, 0, 20);
-// example4.addCir(240, 50, 0, 0, 20);
-// example4.addCir(285, 50, 0, 0, 20);
-// example4.addCir(330, 50, 0, 0, 20);
-// example4.addCir(375, 50, 0, 0, 20);
-// example4.addRect(120, 140, 0, 0, 80, 80, 40, "Blue");
